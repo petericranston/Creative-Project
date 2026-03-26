9bc0a53469d599a71a6391262640d8eae0cd1250
petericranston <39384415+petericranston@users.noreply.github.com>
2026-03-26 18:37:25 +0000

    Removing the content so i can start building the pages

--- Codebase-Health-Monitor/Frontend/src/App.jsx
+++ Codebase-Health-Monitor/Frontend/src/App.jsx
@@ -61,59 +61,7 @@ function App() {
       <header>
         <h1>Codebase Health Monitor</h1>
       </header>
-      <main>
-        <h2>Users Repositories</h2>
-        {repos && repos.map((user, index) => <p key={index}>{user.name}</p>)}
-        <h2>Overview</h2>
-        <p>Repo Name: {overviewData.name}</p>
-        <p>Created at: {overviewData.created_at}</p>
-        <p>Last Updated: {overviewData.updated_at}</p>
-        <p>Most prominent language: {overviewData.language}</p>
-        <h2>Contribution Data</h2>
-        {contributorData &&
-          contributorData.map((user, index) => (
-            <p key={index}>
-              {user.username}: {user.commits} commits
-            </p>
-          ))}
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <ResponsiveContainer width={800} height={300}>
-            <BarChart data={contributorData}>
-              <XAxis dataKey="username" />
-              <YAxis />
-              <Bar dataKey="commits" />
-            </BarChart>
-          </ResponsiveContainer>
-        </div>
-
-        <h2>Dominance Detection</h2>
-        {contributorData &&
-          contributorData.map((user, index) => (
-            <p key={index}>
-              {user.username}: {user.dominanceScore}
-            </p>
-          ))}
-        <h3>Top Contributor</h3>
-        <h4>
-          {contributorData.length > 0 //Displaying the top contributor (the array is already organized based on dominance percentage so I just get the first index)
-            ? contributorData[0].username
-            : "Loading..."}
-        </h4>
-        <h2>Files Content</h2>
-
-        {filesContent &&
-          filesContent.map((file, index) => (
-            <p key={index}>
-              {file.name} - {file.path}
-            </p>
-          ))}
-      </main>
+      <main></main>
     </div>
   );
 }
