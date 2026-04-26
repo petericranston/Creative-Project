c974e7444e57167adb5d21c6f448fa2064701e97
petericranston <39384415+petericranston@users.noreply.github.com>
2026-04-26 20:15:03 +0100

    Fixing styling

--- Codebase-Health-Monitor/Frontend/src/pages/Risk.jsx
+++ Codebase-Health-Monitor/Frontend/src/pages/Risk.jsx
@@ -4,20 +4,11 @@ import "../styles/overview.css";
 
 export default function Risk() {
   return (
-    <div>
+    <div className="flex flex-col h-full">
       <h2 className="text-2xl font-semibold pb-10">Risk</h2>
-      <div className="flex gap-4">
-        <div className="bg-[#272953] w-[400px] h-[330px] flex rounded-lg items-center justify-center flex-col">
-          <p className="text-xl">Choose a file to analyse</p>
-        </div>
-        <div className="bg-[#272953] rounded-lg p-4 text-white flex-1 flex flex-col items-center justify-center">
-          <h2 className="text-4xl">Top Contributor</h2>
-        </div>
-      </div>
-      <div className="flex gap-4 mt-4">
-        <div className="bg-[#272953] rounded-lg p-4 text-white flex-1 flex flex-col items-center justify-center h-[350px] overflow-hidden">
-          <p className="text-base">Lines Added/Removed per Contributor</p>
-        </div>
+      <div className="flex gap-4 flex-1">
+        <div className="bg-[#272953] rounded-lg w-1/3"></div>
+        <div className="bg-[#272953] rounded-lg flex-1"></div>
       </div>
     </div>
   );
